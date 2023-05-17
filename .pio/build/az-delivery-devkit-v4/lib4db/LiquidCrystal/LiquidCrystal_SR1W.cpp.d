550f996c77abab61b1b09282208bcd1a9be4e861
MDFreak_ESP32 <post@martin-dorfner.at>
2023-05-17 09:14:21 +0200

    new cloned for versioning

--- .pio/build/az-delivery-devkit-v4/lib4db/LiquidCrystal/LiquidCrystal_SR1W.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/az-delivery-devkit-v4/lib4db/LiquidCrystal/LiquidCrystal_SR1W.cpp.o: \
- .pio/libdeps/az-delivery-devkit-v4/LiquidCrystal/LiquidCrystal_SR1W.cpp
--- .pio/build/az-delivery-devkit-v4/liba50/md_lcd/md_lcd.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/az-delivery-devkit-v4/liba50/md_lcd/md_lcd.cpp.o: \
- C:/Users/konta/Documents/PlatformIO/Projects/md_lib/md_lcd/src/md_lcd.cpp
